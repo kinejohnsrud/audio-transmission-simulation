cf149cc42f93b9b26afaa4b9fd21ed97d4acef0f
Kine Johnsrud <kinejohnsrud@gmail.com>
2014-02-05 16:56:27 -0800

    Scrap initial code, new methods to read and write audio file
    Rewrite everything. Has read and write method for complete audio file, as well as a network simulation method (with non-functioning “silencing” of lost packets) and a calculateLossProbability to determine how many packets should be “lost”.

--- HW2_Audio/src/Simulator.java
+++ HW2_Audio/src/Simulator.java
@@ -1,73 +1,75 @@
+import java.io.DataInputStream;
 import java.io.File;
-import java.nio.file.spi.FileTypeDetector;
-
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class Simulator {
 	
+	byte[] byteArray;
+	byte[] sampleArray;
+	
 	public static void main(String[] args) {
-
-		//READING A SOUND FILE IN JAVA
-		int totalFramesRead = 0;
-		File fileIn = new File("/Users/Kine/Documents/GitHub/Network_Computing/HW2_Audio/src/canttakeit.au");
-		
+		Simulator run = new Simulator();
 		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileIn);
-			int bytesPerFrame = audioInputStream.getFormat().getFrameSize();
-			int numBytes = 1024 * bytesPerFrame;
-			byte[] audioBytes = new byte[numBytes];
-			
-			try {
-				int numBytesRead = 0;
-				int numFramesRead = 0;
-				
-				while((numBytesRead = audioInputStream.read(audioBytes)) != -1) {
-					numFramesRead = numBytesRead / bytesPerFrame;
-					totalFramesRead += numFramesRead;
-					//HERE: do something useful with the data in the audioBytes array
-					int threshold = 75;
-					for (int i = 0; i < audioBytes.length; i++) {
-						int loss = calculateLossProbability();
-						if(loss < threshold){
-							storeSample(audioBytes[i]);
-						}
-						else{	//what to do when loss happens
-							audioBytes[i] = 0;
-							storeSample(audioBytes[i]);
-						}	
-					}
-				}
-				System.out.println("Bytes per frame: " + bytesPerFrame);
-				System.out.println("Number of bytes: " + numBytes);
-				System.out.println(audioBytes[0] + " and " + audioBytes[1] + " and " + audioBytes[2] + " and " + audioBytes[3] + " and " + audioBytes[4] + " and " + audioBytes[5]);
-			
-				//THE BASICS OF WRITING A SOUND FILE IN JAVA
-//				File fileOut = new File("/Users/Kine/Documents/GitHub/Network_Computing/HW2_Audio/src/copy.au");
-//				AudioFileFormat.Type fileType = AudioFileFormat.Type.AU;
-//				if (AudioSystem.isFileTypeSupported(fileType, audioInputStream)) {
-//					AudioSystem.write(audioInputStream, fileType, fileOut);
-//				}
-			
-			} catch (Exception e){
-				System.out.println("wrong!");
-			}
-		} catch (Exception e){
-			System.out.println("wrong2!");
+			run.readFile();
+			run.networkSimulation(run.byteArray);
+			run.writeFile(run.sampleArray);
 
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("sumfing went wrong");
+		}
+		
+	}
+	
+	public void readFile() throws IOException{
+	    File soundFile = new File("/Users/Kine/Documents/GitHub/Network_Computing/HW2_Audio/src/canttakeit.au");
+	    byteArray = new byte[(int) soundFile.length()];
+	    DataInputStream dis = new DataInputStream(new FileInputStream(soundFile));
+	    dis.readFully(byteArray);
+	    dis.close();
+	}
+	
+	public void networkSimulation(byte[] bArray){
+		sampleArray = new byte[byteArray.length];
+		for (int i = 0; i < bArray.length; i++) {
+			int loss = calculateLossProb();
+			int threshold = 80;
+			System.out.println("Byte: " + bArray[i]);
+			if (loss < threshold){
+				//storeSample(bArray[i]);
+				sampleArray[i] = bArray[i];
+			}
+			else{
+				bArray[i] = 0;
+				sampleArray[i] = bArray[i];
+
+			}
 		}
 	}
+	
+//	private void storeSample(byte b) {	
+//	}
 
-	private static void storeSample(byte b) {
-		// TODO Auto-generated method stub
-		
+	public int calculateLossProb(){
+		int prob = 0;
+		prob = (int)(Math.random()*100);
+		//System.out.println("Prob: " + prob);
+		return prob;
 	}
-
-	//loss probability should be low, medium and high
-	private static int calculateLossProbability() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public void writeFile(byte[] byteArray) throws IOException{
+		//write the output sound file
+		FileOutputStream fos = new FileOutputStream("/Users/Kine/Documents/GitHub/Network_Computing/HW2_Audio/src/output.au");
+		fos.write(byteArray);
+		fos.close();
 	}
+	
+	
+	
+	
 }
