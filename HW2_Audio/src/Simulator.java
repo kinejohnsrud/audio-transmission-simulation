abc062e6ffdd3633f0ba756dd2c8296ef4f7636b
Kine Johnsrud <kinejohnsrud@gmail.com>
2014-02-05 17:19:41 -0800

    Substitute lost byte with last played byte
    It is flawed to use byte, since audio-file will be unplayable if wrong byte is lost. Next: put bytes in packets and handle them that way instead.

--- HW2_Audio/src/Simulator.java
+++ HW2_Audio/src/Simulator.java
@@ -1,7 +1,6 @@
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -17,13 +16,11 @@ public class Simulator {
 			run.readFile();
 			run.networkSimulation(run.byteArray);
 			run.writeFile(run.sampleArray);
-
-			
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("sumfing went wrong");
 		}
-		
 	}
 	
 	public void readFile() throws IOException{
@@ -36,40 +33,34 @@ public class Simulator {
 	
 	public void networkSimulation(byte[] bArray){
 		sampleArray = new byte[byteArray.length];
+		byte lastSampledByte = 103;
+		//one "packet" = one byte
 		for (int i = 0; i < bArray.length; i++) {
 			int loss = calculateLossProb();
 			int threshold = 80;
 			System.out.println("Byte: " + bArray[i]);
 			if (loss < threshold){
-				//storeSample(bArray[i]);
 				sampleArray[i] = bArray[i];
+				lastSampledByte = bArray[i];
 			}
 			else{
-				bArray[i] = 0;
-				sampleArray[i] = bArray[i];
-
+				//bArray[i] = 0;
+				//sampleArray[i] = bArray[i];
+				sampleArray[i] = lastSampledByte;
 			}
 		}
 	}
-	
-//	private void storeSample(byte b) {	
-//	}
 
 	public int calculateLossProb(){
 		int prob = 0;
 		prob = (int)(Math.random()*100);
-		//System.out.println("Prob: " + prob);
 		return prob;
 	}
 	
 	public void writeFile(byte[] byteArray) throws IOException{
-		//write the output sound file
 		FileOutputStream fos = new FileOutputStream("/Users/Kine/Documents/GitHub/Network_Computing/HW2_Audio/src/output.au");
 		fos.write(byteArray);
 		fos.close();
 	}
-	
-	
-	
 	
 }
